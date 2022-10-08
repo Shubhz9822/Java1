fda5124f8163d64bc0342a3c5c68eea44d6ca473
Kaustubh <30626709+armada1996@users.noreply.github.com>
2022-10-08 20:14:04 +0530

    test1
    Skillrary

---
+++ Test1Skillrary.java
@@ -0,0 +1,26 @@
+// 02/08/22
+
+package JavaScriptExecutorADV;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1Skillrary {
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver", "./software/chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://live.skillrary.com/testing-app/");
+		WebElement element = driver.findElement(By.xpath("//input[@class='form-control']"));
+		
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		jse.executeScript("arguments[0].value='pune'", element);
+	}
+}
